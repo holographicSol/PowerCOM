e4e18019736c45f59b33f9c357a9e9244f511216
holographic_sol <88397088+holographicSol@users.noreply.github.com>
2024-09-29 17:36:48 +0100

    Update powercomasync.py

--- powercomasync.py
+++ powercomasync.py
@@ -22,10 +22,12 @@ baud_rates = [
 ]
 
 
-async def read_and_print(aioserial_instance: aioserial.AioSerial, tags: list):
+async def power_com(aioserial_instance: aioserial.AioSerial, tags: list):
+    """ discover sentence tag(s) on a COM port and return discovered sentence(s).
+    (can retrieve multiple differently tagged sentences per port) """
     results = []
     found_tags = []
-    for i in range(0, 10):
+    for i_try_to_get_a_good_read in range(0, 10):
         at_most_certain_size_of_bytes_read: bytes = await aioserial_instance.read_until_async(aioserial.LF, None)
         if at_most_certain_size_of_bytes_read:
             at_most_certain_size_of_bytes_read = at_most_certain_size_of_bytes_read.decode(encoding='utf-8')
@@ -35,29 +37,22 @@ async def read_and_print(aioserial_instance: aioserial.AioSerial, tags: list):
                         found_tags.append(tag)
                         if at_most_certain_size_of_bytes_read not in results:
                             results.append(at_most_certain_size_of_bytes_read)
-    if results:
-        return results
+    return results
 
 
-async def power_com(item: str, **kwargs):
-    # print('\nscanning comport:', item, '(for tags:', kwargs.values(), ')')
-    results = []
+async def power_com_entrypoint(chunks: list, **kwargs) -> list:
+    """ pass a bag of tags to each instance of power_com """
     tags = kwargs.get('tags')
     try:
-        results = await read_and_print(aioserial.AioSerial(port=item, baudrate=115200), tags=tags)
-    except Exception as e:
+        return [await power_com(aioserial.AioSerial(port=item, baudrate=115200), tags=tags) for item in chunks]
+    except Exception as e:  # handle me!
         pass
-    if results:
-        return results
-
-
-async def power_scan_entry_point(chunks: list, **kwargs) -> list:
-    return [await power_com(item, **kwargs) for item in chunks]
 
 
 async def main(_chunks: list, _multiproc_dict: dict):
+    """ create multiple processes in range of com ports """
     async with aiomultiprocess.Pool() as pool:
-        results = await pool.map(power_scan_entry_point, _chunks, _multiproc_dict)
+        results = await pool.map(power_com_entrypoint, _chunks, _multiproc_dict)
     return results
 
 if __name__ == '__main__':
@@ -66,25 +61,29 @@ if __name__ == '__main__':
     if sys.platform.startswith('win'):
         multiprocessing.freeze_support()
 
-    # chunk data
+    # create simple data
     t0 = time.time()
     coms = []
     for i in range(1, 255):
         coms.append('COM' + str(i))
-    _chunks = handler_chunk.chunk_data(data=coms, chunk_size=1)
-    print('\nchunks:', _chunks)
-    print('chunk time:', time.time()-t0)
 
+    # chunk simple data
+    _chunks = handler_chunk.chunk_data(data=coms, chunk_size=1)
+
+    # create a dictionary to pass to each process (master dict would be better)
     _tags = ['$SATCOM', '$GNGGA', '$SNS']
     _multiproc_dict = {'tags': _tags}
 
+    # let it rip!
     print('\nrunning processes:')
     _t_start = time.perf_counter()
     _results = asyncio.run(main(_chunks, _multiproc_dict))
     _t_completion = str(time.perf_counter() - _t_start)
     print('aiomultiprocess time taken:', _t_completion)
 
+    # some post-processing
     print('\nresults:')
+    _results[:] = [item for item in _results if item is not None]
     _results[:] = [item for sublist in _results for item in sublist if item is not None]
     for _ in _results:
         print(_results)
